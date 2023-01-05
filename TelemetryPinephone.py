dc9a8edd78942546296c1eab3930d09e6712dccd
Rob Caudill <rcaudill@gmail.com>
2023-01-05 14:13:09 -0500

    read telemetry json from host data file

--- TelemetryPinephone.py
+++ TelemetryPinephone.py
@@ -2,6 +2,7 @@ import os
 from BaseMQTTPubSub import BaseMQTTPubSub
 import paho.mqtt.client as mqtt
 from time import sleep
+import json
 
 class TelemetryPinephone(BaseMQTTPubSub):
     def __init__(self):
@@ -10,14 +11,20 @@ class TelemetryPinephone(BaseMQTTPubSub):
         self.mqtt_subscribe_topic = os.environ['BASE_MQTT_SUBSCRIBE_TOPIC']
         
     def mqtt_callback(self, client: mqtt.Client, _userdata: dict, msg: str) -> str:
+        # todo: decide what this module should listen for
         print("Message Topic:", msg.topic)
         print("Message Payload:", str(msg.payload.decode("utf-8")))
         
     def publishPinePhoneTelemetry(self):
-        # PinePhone example
-        # with open("/sys/bus/iio/devices/iio\:device2/in_magn_z_raw", "r") as f:
-        # val = f.readlines()
-        telemetry.publish_to_topic(telemetry.mqtt_publish_topic, "1234")
+        try:
+            f = open('data/telemetry_data.json')
+            data = json.load(f)
+            data = json.dumps(data)
+            print(data)
+            telemetry.publish_to_topic(telemetry.mqtt_publish_topic, data)
+            f.close()
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     telemetry = TelemetryPinephone()
